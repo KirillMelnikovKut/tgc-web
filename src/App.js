a53696f54d7e299ce0483c26f2e1a61db160f36f
KUT <126664273+KirillMelnikovKut@users.noreply.github.com>
2024-02-24 21:04:26 +0300

    fdfd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
-
+import Header from './components/Header/Header.jsx';
+import {Route, Routes} from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList.jsx';
+import Form from './components/Form/Form.jsx';
 function App() {
   const{onToogleButton, tg}= useTelegram();
   useEffect( () => {
@@ -10,9 +12,13 @@ function App() {
   })
 
   return (
+    
     <div className="App">
       <Header/>
-        <button onClick={onToogleButton}>toogle</button>
+      <Routes>
+        <Route index element ={<ProductList/>}/>
+        <Route path={'form'} element ={<Form/>}/>
+      </Routes>
     </div>
   );
 }
